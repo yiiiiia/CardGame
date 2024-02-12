7830fae07d22090900dfcb582fd0fc8a567bcff1
Lau, Wing Chau <2872109l@student.gla.ac.uk>
2024-02-12 16:25:50 +0000

    Replace EndTurnClicked.java

--- app/events/EndTurnClicked.java
+++ app/events/EndTurnClicked.java
@@ -3,7 +3,11 @@ package events;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import akka.actor.ActorRef;
+import commands.BasicCommands;
 import structures.GameState;
+import java.util.*;
+
+import org.apache.commons.lang3.RandomStringUtils;
 
 /**
  * Indicates that the user has clicked an object on the game canvas, in this case
@@ -20,7 +24,19 @@ public class EndTurnClicked implements EventProcessor{
 
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
-		
+		//ignorePlayerAction
+		gameState.setIgnoreEvent(true);
+		//clearMana
+		gameState.getAllPlayer[0].setMana(0);
+		//drawCard
+		Random random = new Random(gameState.getAllPlayer[0].getcardsRemain().size());
+		List<Card> cardRemain = gameState.getAllPlayer[0].getCardsRemain();
+		if(cardRemain < 6) {
+			BasicCommands.drawCard(out, cardRemain.get(random.nextInt(cardRemain.size())), cardRemain.size() + 1, 0);
+		}
+		//setAIAsActivePlayer
+		gameState.setAIAsActivePlayer();
+
 	}
 
 }
