28b94dbb8b03a4c6a96a57e16517e023284aca30
Sun, Jiangdong <2840905s@student.gla.ac.uk>
2024-02-13 21:41:29 +0000

    Replace GameState.java

--- app/structures/GameState.java
+++ app/structures/GameState.java
@@ -1,5 +1,8 @@
 package structures;
 
+import structures.basic.Player;
+import structures.basic.Tile;
+
 /**
  * This class can be used to hold information about the on-going game.
  * Its created with the GameActor.
@@ -13,5 +16,31 @@ public class GameState {
 	public boolean gameInitalised = false;
 	
 	public boolean something = false;
+	public Status game_status;
+	public Player[] players=new Player[2];
+	
+	
+	public enum Status
+	{
+		IDLE,
+		UNIT_SELECTED,
+		AI
+		
+		
+	}
+	
+	public Tile[][] boardTile;//后面再变私有
+	public GameState()
+	{boardTile=new Tile[9][5];
+	game_status=Status.IDLE;
+	
+		
+	}
+	
+	
+	
+	
+ 
+	
 	
 }
