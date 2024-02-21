e4cc783deade25b4243b0a6898b8d3e81ff86624
Lau, Wing Chau <2872109l@student.gla.ac.uk>
2024-02-21 22:42:29 +0000

    Upload New File

---
+++ app/structures/basic/unit/Wraithling.java
@@ -0,0 +1,28 @@
+package structures.basic.unit;
+
+import akka.actor.ActorRef;
+import commands.BasicCommands;
+import structures.GameState;
+import structures.basic.Card;
+import structures.basic.ImageCorrection;
+import structures.basic.Position;
+import structures.basic.Tile;
+import structures.basic.Unit;
+import structures.basic.UnitAnimationSet;
+import structures.basic.UnitAnimationType;
+
+public class Wraithling extends Unit {
+    public Wraithling(int id, UnitAnimationSet animations, ImageCorrection correction, Tile currentTile) {
+        super(id, animations, correction, currentTile, 1, 1);
+    }
+
+    public Wraithling(int id, UnitAnimationType animation, Position position, UnitAnimationSet animations,
+                      ImageCorrection correction) {
+        super(id, animation, position, animations, correction, 1, 1);
+    }
+
+    public Wraithling(int id, UnitAnimationSet animations, ImageCorrection correction, int health, int attack) {
+        super(id, animations, correction, health, attack);
+    }
+    
+}
