f4511111318cd19dd21ece588d05ae9433424aa2
Lau, Wing Chau <2872109l@student.gla.ac.uk>
2024-02-23 13:04:25 +0000

    Upload New File

---
+++ app/structures/basic/unit/PlayerAvatar.java
@@ -0,0 +1,15 @@
+package structures.basic.unit;
+
+import structures.basic.Unit;
+
+public class PlayerAvatar extends Unit {
+
+    private int health;
+    private int attack;
+    
+    public PlayerAvatar() {
+        super();
+
+    }
+
+}
