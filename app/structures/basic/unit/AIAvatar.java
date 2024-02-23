78c5db68855cfc61d80de28fa6eadcdb25ee81ad
Lau, Wing Chau <2872109l@student.gla.ac.uk>
2024-02-23 13:04:37 +0000

    Upload New File

---
+++ app/structures/basic/unit/AIAvatar.java
@@ -0,0 +1,13 @@
+package structures.basic.unit;
+
+import structures.basic.Unit;
+
+public class AIAvatar extends Unit {
+    
+    private int health;
+    private int attack;
+
+    public AIAvatar() {
+        super();
+    }
+}
