f694e8acee3b412a874a3b06f39fe1f68a48513b
Lau, Wing Chau <2872109l@student.gla.ac.uk>
2024-02-23 13:14:02 +0000

    Replace WraithlingSwarmCard.java

--- app/structures/basic/card/WraithlingSwarmCard.java
+++ app/structures/basic/card/WraithlingSwarmCard.java
@@ -12,44 +12,44 @@ import structures.basic.unit.Wraithling;
 
 import java.util.List;
 
+//call processEvent in tileClicked
+
 public class WraithlingSwarmCard extends Card {
 
-    private int emptyTileCount;
-    //highlight the unoccupied tiles
+
+    public void performSpell(ActorRef out, GameState gameState, JsonNode message) {
+        //to spawn the <= 3 wraithlings
+        for (int i = 0; i < 3; i++) {
+            List<Tile> emptyTiles = highlightTiles(out, gameState);
+            if(!emptyTiles.isEmpty()) {
+                TileClicked tileClicked = new TileClicked();
+                tileClicked.processEvent(out, gameState, message);
+            }
+        }
+    }
+
+    public void summonUnit(ActorRef out, GameState gameState, JsonNode message) {
+        int tilex = message.get("tilex").asInt();
+		int tiley = message.get("tiley").asInt();
+		Tile tile = gameState.getTileByPos(tilex, tiley);
+        if(tile.getUnit()==null) {
+            Wraithling wraithling = new Wraithling();
+            //Wraithling wraithling = BasicObjectBuilders.loadUnit(StaticConfFiles.wraithling, 1, Unit.class);
+            wraithling.setPositionByTile(tile);
+            BasicCommands.drawUnit(out, wraithling, tile);
+            gameState.getPlayerUnits().put(tile, wraithling);
+        }
+    }   
+
     public List<Tile> highlightTiles(ActorRef out, GameState gameState) {
         List<Tile> emptyTiles = gameState.getAllTiles();
-        
-        for(Tile tile: emptyTiles) {
+        for (Tile tile: emptyTiles) {
             if(tile.getUnit()!=null) {
-            emptyTiles.remove(tile);
+                emptyTiles.remove(tile);
             }else {
-                BasicCommands.drawTile(out,tile,1);
+                BasicCommands.drawTile(out, tile, 1);
             }
         }
         return emptyTiles;
     }
-
-    //to spawn the <= 3 wraithlings
-    public void spawnWraithlings(ActorRef out, GameState gameState, JsonNode message, List<Tile> emptyTiles) {
-        this.emptyTileCount = 3;
-        for(int i = 0; i < emptyTileCount; i++) {
-            this.highlightTiles(out, gameState);
-            TileClicked tileClicked = new TileClicked();
-            int tilex = message.get("tilex").asInt();
-            int tiley = message.get("tiley").asInt();
-            Tile tile = gameState.getTileByPos(tilex, tiley);
-            tileClicked.handleNoUnitOnTile(out, gameState, tile);
-            //if player clicks on one of the tiles in emptyTiles
-            if(emptyTiles.contains(tile)) {
-                Wraithling wraithling = new Wraithling();
-                wraithling.setPositionByTile(tile);
-                BasicCommands.drawUnit(out, wraithling, tile);
-                gameState.getPlayerUnits().put(tile, wraithling);
-                emptyTiles.remove(tile);
-                //check what tile the player clicks
-                //summon a wraithling on that tile if available
-            }
-        }
-    }
-
 }
