17d79c9266c23e8783af7d889bf98908c4497448
Lau, Wing Chau <2872109l@student.gla.ac.uk>
2024-02-24 16:12:23 +0000

    Update AIAvatar.java

--- app/structures/basic/unit/AIAvatar.java
+++ app/structures/basic/unit/AIAvatar.java
@@ -1,13 +1,18 @@
 package structures.basic.unit;
 
 import structures.basic.Unit;
+import utils.BasicObjectBuilders;
 
 public class AIAvatar extends Unit {
     
-    private int health;
-    private int attack;
+    protected int health;
+    protected int attack;
 
     public AIAvatar() {
         super();
+    }
+
+    public static AIAvatar getInstance(String configpaths) {
+        return (AIAvatar)BasicObjectBuilders.loadUnit(configpaths, 1, AIAvatar.class);
     }
 }
