8340431a3afd943ea37328c23b2eaa74ff2b8567
Lau, Wing Chau <2872109l@student.gla.ac.uk>
2024-02-24 15:56:01 +0000

    Update BadOmen.java

--- app/structures/basic/unit/BadOmen.java
+++ app/structures/basic/unit/BadOmen.java
@@ -1,12 +1,14 @@
 package structures.basic.unit;
 
+import commands.BasicCommands;
 import structures.GameState;
 import structures.basic.Unit;
+import utils.BasicObjectBuilders;
 
 public class BadOmen extends Unit {
 
-    private int health;
-    private int attack;
+    protected int health;
+    protected int attack;
 
     public BadOmen() {
         super();
@@ -16,12 +18,11 @@ public class BadOmen extends Unit {
 
     public void performDeathWatch(ActorRef out, GameState gameState) {
         //whenever a unit, friendly or enemy dies
-        int numUnit = gameState.getAllUnits().size();
-        int prevNumUnit = numUnit;
-        if (prevNumUnit > gameState.getAllUnits().size()) {
-            this.setAttack(attack + 1);
-        }
+        this.setAttack(attack + 1);
+        BasicCommands.setUnitAttack(out, this, attack + 1);
     }
 
+    public static BadOmen getInstance(String configpaths) {
+        return (BadOmen)BasicObjectBuilders.loadUnit(configpaths, 0, BadOmen.class);
+    }
 }
-
