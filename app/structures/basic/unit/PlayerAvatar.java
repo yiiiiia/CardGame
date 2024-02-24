2b299432f6ad07861335755525d31dfbe075e2e2
Lau, Wing Chau <2872109l@student.gla.ac.uk>
2024-02-24 16:13:09 +0000

    Update PlayerAvatar.java

--- app/structures/basic/unit/PlayerAvatar.java
+++ app/structures/basic/unit/PlayerAvatar.java
@@ -1,15 +1,18 @@
 package structures.basic.unit;
 
 import structures.basic.Unit;
+import utils.BasicObjectBuilders;
 
 public class PlayerAvatar extends Unit {
 
-    private int health;
-    private int attack;
+    protected int health;
+    protected int attack;
     
     public PlayerAvatar() {
         super();
-
     }
 
+    public static PlayerAvatar getInstance(String configpaths) {
+        return (PlayerAvatar)BasicObjectBuilders.loadUnit(configpaths, 0, PlayerAvatar.class);
+    }
 }
