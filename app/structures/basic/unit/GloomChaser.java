c5f8f3836069a4cc9b2f97dac024ede6bf29fc5f
Lau, Wing Chau <2872109l@student.gla.ac.uk>
2024-02-24 16:22:35 +0000

    Upload New File

---
+++ app/structures/basic/unit/GloomChaser.java
@@ -0,0 +1,31 @@
+package structures.basic.unit;
+
+import commands.BasicCommands;
+import structures.GameState;
+import structures.basic.Tile;
+import structures.basic.Unit;
+import utils.BasicObjectBuilders;
+import utils.StaticConfFiles;
+
+public class GloomChaser extends Unit{
+
+        protected int health;
+        protected int attack;
+    
+    public GloomChaser() {
+        super();
+        health = 1;
+        attack = 3;
+    }
+
+    public void performGambit(ActorRef out, GameState gameState) {
+        Tile tile = gameState.getTileByPos(this.getPosition().getTilex()-1, this.getPosition().getTiley());
+        if(tile.getUnit()!=null) {
+            BasicCommands.playEffectAnimation(out, "f1_wraithsummon", tile);
+            Wraithling wraithling = (Wraithling)BasicObjectBuilders.loadUnit(StaticConfFiles.wraithling, 1, Wraithling.class);
+            wraithling.setPositionByTile(tile);
+            BasicCommands.drawUnit(out, wraithling, tile);
+            gameState.getPlayerUnits().put(tile, wraithling);
+        }
+    }
+}
