2066faf203f8cedcc4d26822867d057ba2b918be
Lau, Wing Chau <2872109l@student.gla.ac.uk>
2024-02-26 22:08:25 +0000

    Replace BloodmoonPriestess.java

--- app/structures/basic/unit/BloodmoonPriestess.java
+++ app/structures/basic/unit/BloodmoonPriestess.java
@@ -1,16 +1,49 @@
-package structures.basic.card;
+package structures.basic.unit;
 
-import akka.actor.ActorRef;
+import java.util.List;
+import java.util.Random;
+
 import commands.BasicCommands;
 import structures.GameState;
-import structures.basic.Card;
-import structures.basic.unit.BloodmoonPriestess;
+import structures.basic.Tile;
+import structures.basic.Unit;
+import utils.BasicObjectBuilders;
+import utils.StaticConfFiles;
 
-public class BloodmoonPriestessCard extends Card {
-    public void summonUnit (ActorRef out, GameState gameState, int tilex, int tiley){
-        BloodmoonPriestess bloodmoonPriestessUnit=new BloodmoonPriestess();
-        bloodmoonPriestessUnit.setPositionByTile(gameState.getGameTiles()[tilex][tiley]);
-        BasicCommands.drawUnit(out, bloodmoonPriestessUnit, gameState.getGameTiles()[tilex][tiley]);
-        gameState.getUserPlayer().getAllUnits().put(gameState.getGameTiles()[tilex][tiley],bloodmoonPriestessUnit);
+public class BloodmoonPriestess extends Unit {
+    
+    public static final int initialHealth = 3;
+    public static final int initialAttack = 3;
+
+    protected int health;
+    protected int attack;
+
+    public BloodmoonPriestess() {
+        super();
+        health = 3;
+        attack = 3;
+    }
+
+    public void performDeathWatch(ActorRef out, GameState gameState) {
+        //whenever a unit, friendly or enemy dies
+        Random r = new Random();
+        List<Tile> emptyTiles = gameState.getAllTiles();
+        for(Tile tile: emptyTiles) {
+            if(tile.getUnit()!=null) {
+                emptyTiles.remove(tile);
+            }
+        }
+        if (emptyTiles.size() > 0) {
+            int randomTile = r.nextInt(emptyTiles.size());
+            BasicCommands.playEffectAnimation(out, "f1_wraithsummon", emptyTiles.get(randomTile));
+            Wraithling wraithling = (Wraithling)BasicObjectBuilders.loadUnit(StaticConfFiles.wraithling, 1, Wraithling.class);
+            wraithling.setPositionByTile(emptyTiles.get(randomTile));
+            BasicCommands.drawUnit(out, wraithling, emptyTiles.get(randomTile));
+            gameState.getPlayerUnits().put(emptyTiles.get(randomTile), wraithling);
+        }
+    }
+
+    public static BloodmoonPriestess getInstance(String configpaths) {
+        return (BloodmoonPriestess)BasicObjectBuilders.loadUnit(configpaths, 7, BloodmoonPriestess.class);
     }
 }
