76de14adbcc1a4c03a60cb55f080a77995466ab6
Sun, Jiangdong <2840905s@student.gla.ac.uk>
2024-03-02 00:12:52 +0000

    Replace OtherClicked.java

--- app/events/OtherClicked.java
+++ app/events/OtherClicked.java
@@ -22,44 +22,21 @@ import utils.BasicObjectBuilders;
  *
  */
 public class OtherClicked implements EventProcessor{
-	public  void PlaceableArea(ActorRef out,GameState gameState,Tile tile,java.util.Map<Tile, Unit> map,int type)
-	{
-		int x=tile.getTilex()-1;
-		int y=tile.getTiley()-1;
-		for(int i=0;i<3;i++)
-	{for(int j=0;j<3;j++)
-		{if(isPlaceBle(x+i,y+j,gameState))
-		{
-			BasicCommands.drawTile(out, gameState.boardTile[x+i][y+j], type);
-		}
-		
-		}}
-	}
 	
-	public boolean isPlaceBle(int x,int y,GameState gameState)
-	{if(x<0||x>8||y<0||y>4||gameState.players[0].getUnit().containsKey(gameState.boardTile[x][y]))//这里得再加AI类的unit判断
-	{return false;}
-	return true;
-	}
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
-		
-		
-		
-		for(int i=0;i<gameState.players[0].getNumCard();i++)//后续在gamestate里面加属性精准定位。
+		if(gameState.getActivateCard()!=null)
 		{
-			BasicCommands.drawCard(out, gameState.players[0].getCard()[i], i+1, 0);
+			{for(Tile cur:gameState.getUserPlayer().getAllUnitsAndTile().keySet())
+			{
+				gameState.PlaceableArea(out,cur,0);
+				
+			}
+			}
+			gameState.setActivateCard(null);
 		}
 		
-		for(Tile cur:gameState.players[0].getUnit().keySet())
-		{
-			PlaceableArea(out,gameState,cur,gameState.players[0].getUnit(),0);
-			
-		}
-		for(Tile cur:gameState.players[1].getUnit().keySet())
-		{
-			BasicCommands.drawTile(out, cur, 0);
-		}
+		
 		
 	}
 
